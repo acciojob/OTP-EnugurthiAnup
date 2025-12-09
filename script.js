44a8f499fdee10000d5676b87dcec1c7c3fd7efa
EnugurthiAnup <eanup.736@gmail.com>
2025-12-09 23:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+
+    input.addEventListener('input', (e) => {
+        const value = e.target.value;
+
+        // Allow only numbers
+        e.target.value = value.replace(/[^0-9]/g, "");
+
+        if (value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus(); // move to next
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === "Backspace") {
+            if (input.value === "") {
+                // go to previous input if empty
+                if (index > 0) {
+                    inputs[index - 1].value = "";
+                    inputs[index - 1].focus();
+                }
+            } else {
+                // clear current box first
+                input.value = "";
+            }
+        }
+    });
+});
